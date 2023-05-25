5ff3e0a8b1f41d43ed5a6de8daa90698d54b7e04
ramiinberg <rami.inberg@gmail.com>
2023-05-25 13:02:54 +0300

    BaseViewModel

--- Part 1 - Displaying Data/MonkeyFinder/ViewModel/BaseViewModel.cs
+++ Part 1 - Displaying Data/MonkeyFinder/ViewModel/BaseViewModel.cs
@@ -1,5 +1,25 @@
 ﻿namespace MonkeyFinder.ViewModel;
 
-public class BaseViewModel
+public class BaseViewModel : INotifyPropertyChanged
 {
+    bool isBusy;
+    string title;
+
+    public bool IsBusy
+    {
+        get => IsBusy;
+        set
+        {
+            if (isBusy == value) return;
+            isBusy = value;
+            OnPropertyChanged("IsBusy");
+        }
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    public void OnPropertyChanged(string name)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
 }
